7672dbfd77a3889c92ee953aca8c850f270b4fe1
Arjunmjbhr <arjunmjbhr@gmail.com>
2023-05-10 15:19:43 +0530

    print test

--- Conditional_statements/Lottery.py
+++ Conditional_statements/Lottery.py
@@ -14,18 +14,19 @@ lotteryDigit2 = lottery % 10
 guessDigit1 = guess // 10
 guessDigit2 = guess % 10
 
+
 print("The lottery number is", lottery)
 
 # Check the guess
 if guess == lottery:
     print("Exact match: you win $10,000")
 elif (guessDigit2 == lotteryDigit1 and \
-      guessDigit1 == lotteryDigit2):
+    guessDigit1 == lotteryDigit2):
     print("Match all digits: you win $3,000")
 elif (guessDigit1 == lotteryDigit1
-      or guessDigit1 == lotteryDigit2
-      or guessDigit2 == lotteryDigit1
-      or guessDigit2 == lotteryDigit2):
+    or guessDigit1 == lotteryDigit2
+    or guessDigit2 == lotteryDigit1
+    or guessDigit2 == lotteryDigit2):
     print("Match one digit: you win $1,000")
 else:
     print("Sorry, no match")
